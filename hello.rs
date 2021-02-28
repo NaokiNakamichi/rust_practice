968aad9d90368a5a68c310ff8b1bbfabb13bdbf3
Naoki Nakamichi <ronnao0516@gmail.com>
2021-02-28 23:59:16 +0900

    hoge

--- hello.rs
+++ hello.rs
@@ -1,11 +1,37 @@
 // This is the main function
 // main関数です
-fn main() {
-    // Statements here are executed when the compiled binary is called
-    // コンパイルされたバイナリが実行されるとこの関数が呼び出されます
 
-    // Print text to the console
-    // コンソールに文字列を出力する
-    let x = 5 + /* 90 + */ 5;
-    println!("Is `x` 10 or 100? x = {}", x);
+// Unlike C/C++, there's no restriction on the order of function definitions
+// C/C++とは違い、関数の定義を行う順番に制限はない。
+fn main() {
+    // Variables can be type annotated.
+    // 変数に型を指定
+    let logical: bool = true;
+    printl
+
+    // let a_float: f64 = 1.0;  // Regular annotation
+    //                          // 通常の型指定
+    // let an_integer   = 5i32; // Suffix annotation
+    //                          // サフィックスによる型指定
+
+    // // Or a default will be used.
+    // // サフィックスを指定しない場合、デフォルトを選択
+    // let default_float   = 3.0; // `f64`
+    // let default_integer = 7;   // `i32`
+
+    // // A type can also be inferred from context
+    // let mut inferred_type = 12; // Type i64 is inferred from another line
+    // inferred_type = 4294967296i64;
+
+    // // A mutable variable's value can be changed.
+    // let mutable = 12; // Mutable `i32`
+    //                       // ミュータブルな `i32`.
+    // mutable = 21;
+
+    // // Error! The type of a variable can't be changed.
+    // // エラー！ ミュータブルな変数でも型は不変
+    // mutable = true;
+
+    // // Variables can be overwritten with shadowing.
+    // let mutable = true;
 }
