63afca1d3a758e4e5321dd3db4992469b8521d76
Naoki Nakamichi <ronnao0516@gmail.com>
2021-02-28 23:11:07 +0900

    first commit

---
+++ hello.rs
@@ -0,0 +1,11 @@
+// This is the main function
+// main関数です
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    // コンパイルされたバイナリが実行されるとこの関数が呼び出されます
+
+    // Print text to the console
+    // コンソールに文字列を出力する
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
