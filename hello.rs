c58bb322205f69f07d162373c64ed69cb03a98b0
Naoki Nakamichi <ronnao0516@gmail.com>
2021-03-01 21:39:16 +0900

    tmp

--- hello.rs
+++ hello.rs
@@ -7,26 +7,22 @@ fn main() {
     // Variables can be type annotated.
     // 変数に型を指定
     let logical: bool = true;
-    printl
+    println!("{}",logical);
 
-    // let a_float: f64 = 1.0;  // Regular annotation
-    //                          // 通常の型指定
-    // let an_integer   = 5i32; // Suffix annotation
-    //                          // サフィックスによる型指定
+    let an_integer   = 5i32;
+    println!("{}",an_integer);
 
-    // // Or a default will be used.
-    // // サフィックスを指定しない場合、デフォルトを選択
-    // let default_float   = 3.0; // `f64`
-    // let default_integer = 7;   // `i32`
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
 
-    // // A type can also be inferred from context
-    // let mut inferred_type = 12; // Type i64 is inferred from another line
-    // inferred_type = 4294967296i64;
+    println!("{}",inferred_type);
 
-    // // A mutable variable's value can be changed.
-    // let mutable = 12; // Mutable `i32`
-    //                       // ミュータブルな `i32`.
-    // mutable = 21;
+
+    let mut mutable = 12; // Mutable `i32`
+                          // ミュータブルな `i32`.
+    mutable = 21i64;
+
+    println!("{}",mutable);
 
     // // Error! The type of a variable can't be changed.
     // // エラー！ ミュータブルな変数でも型は不変
@@ -34,4 +30,5 @@ fn main() {
 
     // // Variables can be overwritten with shadowing.
     // let mutable = true;
+    let mutable = true;
 }
